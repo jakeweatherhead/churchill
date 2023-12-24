2d48527e603ffb78146e1020ced6aabb40418b9f
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-12-24 11:34:52 +0200

    Moved PCP_Strategy_0 to PutCallParity folder

---
+++ src/PutCallParity/PCP_Strategy_0.cpp
@@ -0,0 +1,40 @@
+#include "PutCallParity/PCP_Strategy_0.h"
+
+std::vector<OptionPair> PCP_Strategy_0::filterArbitrageOpportunities(std::vector<OptionPair> &candidates)
+{
+    std::vector<OptionPair> optionPairs;
+    for (OptionPair &optionPair : candidates)
+    {
+        setCapitalRequired(optionPair);
+        setProfit(optionPair);
+        setReturnPerc(optionPair);
+        if (returnIsSufficient(optionPair))
+        {
+            optionPairs.push_back(optionPair);
+        }
+    }
+    return optionPairs;
+}
+
+void PCP_Strategy_0::setCapitalRequired(OptionPair &optionPair)
+{
+    optionPair.capitalRequired = optionPair.callPrice;
+    optionPair.capitalRequired += optionPair.makerFee; 
+    optionPair.capitalRequired += optionPair.takerFee;
+    optionPair.capitalRequired += optionPair.deribitFuturesMidPrice;
+}
+
+void PCP_Strategy_0::setProfit(OptionPair &optionPair)
+{
+    optionPair.profit = optionPair.callPrice;
+    optionPair.profit -= optionPair.makerFee;
+    optionPair.profit += optionPair.strike;
+    optionPair.profit -= optionPair.putPrice;
+    optionPair.profit -= optionPair.takerFee;
+    optionPair.profit -= optionPair.deribitFuturesMidPrice;
+}
+
+bool PCP_Strategy_0::returnIsSufficient(OptionPair &optionPair)
+{
+    return optionPair.returnPerc > 0.01;
+}
