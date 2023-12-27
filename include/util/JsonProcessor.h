cca5cd4ac051bffd9c368bac3b4d9c8fa2c541bc
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-12-27 10:26:02 +0200

    Add include for OptionPair.h and update convertToString function signature

--- include/util/JsonProcessor.h
+++ include/util/JsonProcessor.h
@@ -1,6 +1,8 @@
 #ifndef JSON_PROCESSOR_H
 #define JSON_PROCESSOR_H
 
+#include "OptionPair.h"
+
 #include <string>
 #include <jansson.h>
 #include <nlohmann/json.hpp>
@@ -12,7 +14,7 @@ public:
     static double parseNumber(json_t *value, const char *key);
     static int parseInteger(json_t *value, const char *key);
     static std::string parseString(json_t *value, const char *key);
-    std::string convertToString(const std::vector<OptionPair>& candidates);
+    static std::string convertOptionPairsToString(const std::vector<OptionPair>& candidates);
 };
 
 #endif // JSON_PROCESSOR_H
