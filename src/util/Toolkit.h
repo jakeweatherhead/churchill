62a717ac5a19117b93b6b94a157b98ed4195c34a
jakeweatherhead <jakeweatherhead1@gmail.com>
2024-01-07 13:41:32 +0200

    Update writeToFile function to writeJsonToFile

--- src/util/Toolkit.h
+++ src/util/Toolkit.h
@@ -14,7 +14,7 @@ public:
     Toolkit();
     ~Toolkit();
     static std::string convertDeltaExpirationToDeribit(const std::string &deltaExpiration);
-    static void writeToFile(const std::string &filename, const std::string &data);
+    static void writeJsonToFile(const std::string &filename, const std::string &data);
     static void sortOptionPairsByReturnPerc(std::vector<OptionPair> &optionPairs);
     static std::string toLower(const std::string &str);
     static std::string toUpper(const std::string &str);
